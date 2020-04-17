87e6f1b41a6ed61824217cb9a2d362a3af62bcb1
Majid Shockoohi <majid.shockoohi@gmail.com>
2020-04-17 05:34:00 +0430

    Exe2 done!

---
+++ lab6/p2/routes/users.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var router = express.Router();
+
+router.use(bodyParser.urlencoded({extended:true}));
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+
+router.post('/',function(req,res,next){
+  let {firstname,lastname} =req.body;
+  console.log(`First name: ${firstname}\nLast name: ${lastname}`);
+  res.send('POST Recieved!');
+});
+
+module.exports = router;
