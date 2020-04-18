f96d0d385f993c8e89162534a8a6b101efab4656
Majid Shockoohi <majid.shockoohi@gmail.com>
2020-04-19 01:07:26 +0430

    Lab7 done!

---
+++ lab7/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import FuncStudentList from './FuncStudentList';
+
+function App() {
+  return (
+    <>
+      <FuncStudentList/>
+    </>
+  );
+}
+
+export default App;
